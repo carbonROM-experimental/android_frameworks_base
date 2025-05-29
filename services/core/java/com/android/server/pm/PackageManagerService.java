d8bd4c0c6976361db884d1121c1419da7f471e0e
SirRGB <sirrgb@proton.me>
2025-05-29 22:43:58 +0200

    Revert "PackageManager: Add configuration to specify vendor platform signatures"
    This reverts commit d4b7f54b933c5ab02a92ff3c2327ba476b1c2522.
    
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1419,8 +1419,6 @@ public class PackageManagerService extends IPackageManager.Stub
     private final PackageUsage mPackageUsage = new PackageUsage();
     private final CompilerStats mCompilerStats = new CompilerStats();
 
-    private final Signature[] mVendorPlatformSignatures;
-
     class PackageHandler extends Handler {
 
         PackageHandler(Looper looper) {
@@ -2433,14 +2431,6 @@ public class PackageManagerService extends IPackageManager.Stub
         }
     }
 
-    private static Signature[] createSignatures(String[] hexBytes) {
-        Signature[] sigs = new Signature[hexBytes.length];
-        for (int i = 0; i < sigs.length; i++) {
-            sigs[i] = new Signature(hexBytes[i]);
-        }
-        return sigs;
-    }
-
     public PackageManagerService(Context context, Installer installer,
             boolean factoryTest, boolean onlyCore) {
         LockGuard.installLock(mPackages, LockGuard.INDEX_PACKAGES);
@@ -2453,9 +2443,6 @@ public class PackageManagerService extends IPackageManager.Stub
         }
 
         mContext = context;
-
-        mVendorPlatformSignatures = createSignatures(context.getResources().getStringArray(
-                    R.array.config_vendorPlatformSignatures));
 
         mFactoryTest = factoryTest;
         mOnlyCore = onlyCore;
@@ -9302,19 +9289,6 @@ public class PackageManagerService extends IPackageManager.Stub
         try {
             Trace.traceBegin(TRACE_TAG_PACKAGE_MANAGER, "collectCertificates");
             PackageParser.collectCertificates(pkg, skipVerify);
-            if (compareSignatures(pkg.mSigningDetails.signatures,
-                  mVendorPlatformSignatures) == PackageManager.SIGNATURE_MATCH) {
-                // Overwrite package signature with our platform signature
-                // if the signature is the vendor's platform signature
-                if (mPlatformPackage != null) {
-                    pkg.mSigningDetails = mPlatformPackage.mSigningDetails;
-                    pkg.applicationInfo.seInfo = SELinuxMMAC.getSeInfo(
-                        pkg,
-                        pkg.isPrivileged(),
-                        pkg.applicationInfo.targetSandboxVersion,
-                        pkg.applicationInfo.targetSdkVersion);
-                }
-            }
         } catch (PackageParserException e) {
             throw PackageManagerException.from(e);
         } finally {
@@ -9577,7 +9551,7 @@ public class PackageManagerService extends IPackageManager.Stub
                             disabledPkgSetting /* pkgSetting */, null /* disabledPkgSetting */,
                             null /* originalPkgSetting */, null, parseFlags, scanFlags,
                             (pkg == mPlatformPackage), user);
-                    applyPolicy(pkg, parseFlags, scanFlags, mPlatformPackage, mVendorPlatformSignatures);
+                    applyPolicy(pkg, parseFlags, scanFlags, mPlatformPackage);
                     final ScanResult scanResult = scanPackageOnlyLI(request, mFactoryTest, -1L);
                     if (scanResult.existingSettingCopied && scanResult.request.pkgSetting != null) {
                         scanResult.request.pkgSetting.updateFrom(scanResult.pkgSetting);
@@ -11152,7 +11126,7 @@ public class PackageManagerService extends IPackageManager.Stub
 
         scanFlags = adjustScanFlags(scanFlags, pkgSetting, disabledPkgSetting, user, pkg);
         synchronized (mPackages) {
-            applyPolicy(pkg, parseFlags, scanFlags, mPlatformPackage, mVendorPlatformSignatures);
+            applyPolicy(pkg, parseFlags, scanFlags, mPlatformPackage);
             assertPackageIsValid(pkg, parseFlags, scanFlags);
 
             SharedUserSetting sharedUserSetting = null;
@@ -11776,8 +11750,7 @@ public class PackageManagerService extends IPackageManager.Stub
      * ideally be static, but, it requires locks to read system state.
      */
     private static void applyPolicy(PackageParser.Package pkg, final @ParseFlags int parseFlags,
-            final @ScanFlags int scanFlags, PackageParser.Package platformPkg,
-            Signature[] vendorPlatformSignatures) {
+            final @ScanFlags int scanFlags, PackageParser.Package platformPkg) {
         if ((scanFlags & SCAN_AS_SYSTEM) != 0) {
             pkg.applicationInfo.flags |= ApplicationInfo.FLAG_SYSTEM;
             if (pkg.applicationInfo.isDirectBootAware()) {
@@ -11873,11 +11846,8 @@ public class PackageManagerService extends IPackageManager.Stub
 
         // Check if the package is signed with the same key as the platform package.
         if (PLATFORM_PACKAGE_NAME.equals(pkg.packageName) ||
-                (platformPkg != null && (compareSignatures(
+                (platformPkg != null && compareSignatures(
                         platformPkg.mSigningDetails.signatures,
-                        pkg.mSigningDetails.signatures) == PackageManager.SIGNATURE_MATCH) ||
-                 compareSignatures(
-                        vendorPlatformSignatures,
                         pkg.mSigningDetails.signatures) == PackageManager.SIGNATURE_MATCH)) {
             pkg.applicationInfo.privateFlags |=
                 ApplicationInfo.PRIVATE_FLAG_SIGNED_WITH_PLATFORM_KEY;
